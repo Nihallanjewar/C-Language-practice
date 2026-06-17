58ba54433bfef2faa70b941f5c6e9f749fe3e7fa
Nihallanjewar <nihallanjewar3@gmail.com>
2026-06-17 16:33:41 +0530

    Update 19_hollow_triangle_inverted.c and 20_hollow_ rectangle.c program

---
+++ Nested Loops/19_hollow_triangle_inverted.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = i; j >= 1; j--)
+        if (i == 1 || i == 5 || j == 1 || j == i)
+        {
+            printf("*");
+        }
+        else 
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
